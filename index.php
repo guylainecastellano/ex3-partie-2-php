1710077da41a25a6bc02c5ac1aa7f929309553a3
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-07 14:12:05 +0200

    ex3 partie2 php

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
-$gender = 'gg';
+$gender = 'femme';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
-  <title>ex2 partie2 php</title>
+  <title>ex3 partie2 php</title>
 </head>
 <body>
-  <p>
-    <?php
-    if ($gender =='homme'){
-      echo 'c\est un développeur!!!';
-    }  elseif ($gender =='femme'){
-      echo 'c\est une développeuse!!!';
-    }else {
-      echo 'vous avez fait une erreur';
-    }
-    ?>
-  </p>
+  <?php
+  if ($gender != 'homme'){ ?>
+    <p>c'est une développeuse!!!</p>
+  <?php } else { ?>
+    <p>c'est un développeur!!!</p>
+  <?php } ?>
 </body>
 </html>
